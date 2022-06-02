b67d6bc6137c3f355c4a7579293ea31bb1b98c98
ahmet <ahmetaykun@gmail.com>
2022-06-02 20:32:48 +0300

    ilk mesaj

---
+++ src/main/kotlin/1_getting_started/HelloWorld.kt
@@ -0,0 +1,25 @@
+package `1_getting_started`
+
+
+//fun main() {
+ //   3.14.pow(2.1)
+//}
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
+
+//TEkil yorum satırı
+
+// Todo (ahmet) daha sonra düzeltilecek
+
+
+
+/*Çoklu yorum*/
+
+
+
+/**
+ * dökümantasyon yorum satırı
+ */
+
---
+++ src/main/kotlin/1_getting_started/Variables.kt
@@ -0,0 +1,30 @@
+package `1_getting_started`
+
+fun main() {
+    //val değişmeyen
+    //var değişen
+
+    val name: String = "Ahmet"
+    var surName = "Aykun"
+
+    print(name)
+    println(surName)
+
+    surName = "Tezel"
+
+    // TODO (ahmet) todo özel bir isim
+    println(name)
+    println(surName)
+
+    val age = 3200
+
+
+    val egeColor: String = "asdv22"
+
+    var skinTon = "black"
+    //  skinTon = "Black"
+
+
+    val  number:Int? =null
+
+}
