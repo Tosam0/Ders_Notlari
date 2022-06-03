8a30a58440a3643d1a24c8f565db4ba22eee71fd
ahmet <ahmetaykun@gmail.com>
2022-06-03 20:31:46 +0300

    mesaj 2

---
+++ src/main/kotlin/2 Basics/1 Number.kt
@@ -0,0 +1,28 @@
+package `2 Basics`
+
+fun main(){
+
+    val a :Double= 0.1111111111112223
+
+    val minByteValue: Byte= Byte.MIN_VALUE
+    val maxByetValue:Byte=Byte.MAX_VALUE
+
+    val b = 3.2F
+
+    val c =2312312L
+
+    val d :Int?= null
+    val x=3.1223e1
+
+    val hexadecimalNumber =0x756
+
+
+    val number:Int=10000
+    println(number == number)
+    val boxerNumber:Int? =number
+
+
+
+
+
+}
