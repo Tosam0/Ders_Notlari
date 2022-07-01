d453eab82d2cdf660450b8f46e118f28b3db71f7
ahmet <ahmetaykun@gmail.com>
2022-07-01 17:53:29 +0300

    mesaj 13

---
+++ src/main/kotlin/4-Class/8 DataClass.kt
@@ -0,0 +1,132 @@
+package `4-Class`
+
+class Media
+
+/**
+ * Data clas'en azindan mutlaka 1 paremetre almak zorundadir.
+ * Paremetreler mtlaka val yada var ile tanımlanır.
+ * Open, abstract, sealed, inner class yapılmaz.
+ *Tüm data class'lar final olduğu için final modifier'i redundant uyarısı verir.
+ *
+ *equals(equls-okunuş), heshCode , toString,copy, componenetN fonksiyonları arka planda defoult olarak oluşturulurlar.
+ *
+ *Yukarıdaki fonksiyonlardan biri override edilirse, oto generate edilen hali yazılmaz.
+ *
+ *compenentN fonksiyonlari sebebiyle destructturing declaration olarak data class'lar set edilebiliniyor.
+ *
+ *toString gibi fonksiyonlar primary const. değişkenler kullandiği için val yada var yazılmak zorundadır.
+ *
+ * Pair, Triple ozellestirilmis generic data class'lar mevcut.
+ */
+
+data class News(//parantezin içindekilere componenetN denir.
+    val title: String = "Title",
+    val description: String,
+    val hasMediaContent: Boolean,
+    val mediaList: List<Media>
+
+) {
+
+    val relatedNewsList: List<News> = arrayListOf()
+
+    override fun toString(): String {
+        return "super.toString()"
+    }
+
+    fun log() {
+        println("Title: $title, description :$description")
+    }
+}
+
+/**
+ * İki Clası aynı gibi duruyor farkları ise
+ */
+class NewsData(
+    val title: String = "Title",
+    val description: String,
+    val hasMediaContent: Boolean,
+    val mediaList: List<Media>
+) {
+    val relatedNewsList: List<News> = arrayListOf()
+
+    override fun toString(): String {
+        return "Title:" + this.title + "description=" + this.description + "hasMediaContent=" + this.hasMediaContent + "mediaList:" + this.mediaList
+    }
+
+    fun copy(): NewsData {
+        return NewsData(title, description, hasMediaContent, mediaList)
+    }
+}
+
+fun main() {
+    val newsOne = News(
+        title = "Ynei bir eğitim",
+        description = "Eğitim dönemi yeni bir şekli",
+        hasMediaContent = true,
+        mediaList = arrayListOf()
+    ) //Farklı yazılış yöntemi yukarıdaki ile aynısını yapar.
+//    val newsOne1 = News(
+//        "Ynei bir eğitim",
+//        "Eğitim dönemi yeni bir şekli",
+//        true,
+//        arrayListOf()
+//    )
+    val newsTwoDuzClass = NewsData(
+        title = "Ynei bir eğitim",
+        description = "Eğitim dönemi yeni bir şekli",
+        hasMediaContent = true,
+        mediaList = arrayListOf()
+    ) //Name argumentleri vermek zorunda değiliz aşşağıdaki farklı yöntemi
+//  val newsTwoDuzClass1 = NewsData(
+//        "Ynei bir eğitim",
+//         "Eğitim dönemi yeni bir şekli",ü
+//       true,
+//      arrayListOf()
+//    )
+    //"destructturing declaration" aşağudaki
+    val (title, description, hasMediaContent, mediaList) = newsOne
+    val (title1, description1, hasMediaContent1) = newsOne
+    val (title2, description2) = newsOne
+
+    newsOne.mediaList
+    mediaList
+
+    //val (title1, description1, hasMediaContent1, mediaList1) = newsTwoDuzClass //Düz clasta yapılamaz
+
+    /**  equals = değişkenlerini karşılaştırma yapılıyor.
+     *   hashCode = Classların oluşturduğu hashCodeStringi var bağzen elinizdeki objenin
+    gerçekten elinizdeik diğer objenin birebir aynısı olup olmadığını karşılaştırmak için yapılır.
+     *   toString = clasın içerdiği verileri almak için.
+     *   copy = ise o class'ın bire bir aynısını oluşturmamıza yariyacak.
+     */
+    newsOne.toString()
+    newsOne.hashCode()
+
+    val newsThree = newsOne.copy(description = "Farklı description")
+    val newsFour = News(
+        title = newsOne.title,
+        description = "Farklı",
+        hasMediaContent =newsOne.hasMediaContent,
+        mediaList = newsOne.mediaList
+        )
+
+     newsOne.component1()
+
+    newsOne.relatedNewsList
+    newsOne.title
+
+    // newsTwoDuzClass.equals()
+    newsTwoDuzClass.toString()
+    newsTwoDuzClass.hashCode()
+    //  newsTwoDuzClass.copy()
+    //  newsTwoDuzClass.compo
+
+    println("Data Class toString: $newsOne")
+    println("Data toString: $newsTwoDuzClass")
+
+    val pair = Pair("ahmet", "aykun")
+    val (firet, asdasd, asdas) = Triple("ahmet", "aykun", 23)
+
+
+}
+
--- src/main/kotlin/deneme_Tahtası/deneme2.kt
+++ src/main/kotlin/deneme_Tahtası/deneme2.kt
@@ -1,92 +1,39 @@
 package deneme_Tahtası
 
 fun main() {
-    val denbem = denemeClas("deneme", "aykun")
-    denbem.indexFun("")
-}
-
-open class ClassDeneme(name: String) {
-    var Name1 = name
-
-    open infix fun indexFun(name1: String) {
-        println("Name : $Name1")
-    }
-
-    fun Plus(plus: Int): Int {
-        return plus - plus
-    }
-
-    constructor(name: String, surname: String) : this(name) {
-    }
 
 }
 
-class denemeClas(name: String, surname: String) : ClassDeneme(name) {
-
-    override fun indexFun(name1: String) {
-        super.indexFun(name1)
-        val name1 = "asd"
-    }
-}
-
-abstract class ABSclass {
-    abstract val index1: İndex1Clas
-    abstract val index2: İndex2Clas
-    abstract val index3: İndex3Clas
-
-    abstract fun ABSFun(names: String,yas: Int)
-    val denme: İndex1Clas = İndex1Clas()
-
-    abstract infix fun inxAbstract(soyisim: String)
-    open fun normFun(sayı: Int) {
-        return
-    }
-}
-class NormClas1(names: String) : ABSclass(){
-    override val index1: İndex1Clas
+data class NewDataClass(
+    val name: String,
+    val surname: String = "Aykun",
+    val age: Int,
+    val menu: List<String>
+) : ABSclass() {
+    infix fun String.Pluss(artı: String): Int = this.toInt() + artı.toInt()
+    override val Index1: String
         get() = TODO("Not yet implemented")
-    override val index2: İndex2Clas
+    override val Index2: Int
         get() = TODO("Not yet implemented")
-    override val index3: İndex3Clas
+    override val Index3: Double
         get() = TODO("Not yet implemented")
 
-    override fun ABSFun(names: String,yas: Int) {
+    override fun Absfun(click: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun inxAbstract(soyisim: String) {
-        TODO("Not yet implemented")
-    }
+
 }
+abstract class ABSclass() {
+    abstract val Index1: String
+    abstract val Index2: Int
+    abstract val Index3: Double
+    abstract fun Absfun(click: Int)
 
-abstract class ABSclassAlt1 : ABSclass() {
-    override val index1: İndex1Clas
-        get() = TODO("Not yet implemented")
-    override val index2: İndex2Clas
-        get() = TODO("Not yet implemented")
 
-    override fun ABSFun(names: String,yas: Int) {
-        TODO("Not yet implemented")
-    }
-}
-
-class NormClass2(name1: String):ABSclassAlt1(){
-    override val index3: İndex3Clas
-        get() = TODO("Not yet implemented")
-
-    override fun inxAbstract(soyisim: String) {
-        TODO("Not yet implemented")
-    }
 }
 
 
-class İndex3Clas
-
-class İndex2Clas
-
-class İndex1Clas
-
-
-
-
-
+//open class deneme(name: String) :NewDataClass(name, surname = ){
+//
+//}
